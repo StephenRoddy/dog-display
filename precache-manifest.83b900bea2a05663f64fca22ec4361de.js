54ced385093d716996d3d16942033c73152c4d25
Stephen Roddy <roddyst@tcd.ie>
2020-08-24 18:26:05 +0100

    Updates

---
+++ precache-manifest.83b900bea2a05663f64fca22ec4361de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbc22c6d017b74468785328f18f8ce0",
+    "url": "/dog-display/index.html"
+  },
+  {
+    "revision": "a8d83b3f89cb3a9379e1",
+    "url": "/dog-display/static/css/main.d6791130.chunk.css"
+  },
+  {
+    "revision": "2e5f5df7a11bef8d2a23",
+    "url": "/dog-display/static/js/2.ec16d615.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog-display/static/js/2.ec16d615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d83b3f89cb3a9379e1",
+    "url": "/dog-display/static/js/main.fa612c38.chunk.js"
+  },
+  {
+    "revision": "3a8d254fb5ded1e1dcf8",
+    "url": "/dog-display/static/js/runtime-main.5a240f5f.js"
+  },
+  {
+    "revision": "08b25072ea66fc17ef6e33287b472d39",
+    "url": "/dog-display/static/media/frodo.08b25072.jpg"
+  },
+  {
+    "revision": "37a6c544c9712767c72d64882b2d2029",
+    "url": "/dog-display/static/media/logo.37a6c544.png"
+  }
+]);
